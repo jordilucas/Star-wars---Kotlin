344043c3d0f2e45288800993810e060300227c0c
Jordi Lucas <jordi@joyjet.com>
2017-05-19 14:15:14 -0300

    Correcao nome da variavel result

--- app/src/main/java/com/example/jordisantos/starwars/model/api/DataClassesWeb.kt
+++ app/src/main/java/com/example/jordisantos/starwars/model/api/DataClassesWeb.kt
@@ -6,7 +6,7 @@ import com.google.gson.annotations.SerializedName
  * Created by jordisantos on 19/05/17.
  */
 
-data class FilmResult(val result : List<Film>)
-data class Film(val title : String, @SerializedName("epsiode_id")val episodeId : Int,
+data class FilmResult(val results : List<Film>)
+data class Film(val title : String, @SerializedName("episode_id")val episodeId : Int,
                 @SerializedName("characters")val personUrls : List<String>)
 data class Person(val name : String, val gender : String)
