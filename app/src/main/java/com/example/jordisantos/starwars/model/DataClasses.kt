a0a059a6899b18875670a703e13cf6c651febc99
Jordi <jordilucas3@gmail.com>
2017-05-19 21:54:18 -0300

    mudanca do tipo da lista de characters

--- app/src/main/java/com/example/jordisantos/starwars/model/DataClasses.kt
+++ app/src/main/java/com/example/jordisantos/starwars/model/DataClasses.kt
@@ -4,7 +4,7 @@ package com.example.jordisantos.starwars.model
  * Created by jordisantos on 19/05/17.
  */
 
-data class Movie(val title : String, val episodeId : Int, val characters : List<Character>)
+data class Movie(val title : String, val episodeId : Int, val characters : MutableList<Character>)
 
 data class Character(val name : String, val gender : String){
     override fun toString(): String {
